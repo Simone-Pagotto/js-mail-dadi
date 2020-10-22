212d855caeb5c46cb8749a76f469b19f41791e70
Simone <simone.pagotto.kg@gmail.com>
2020-10-22 17:24:17 +0200

    implementato found

--- js/script.js
+++ js/script.js
@@ -7,31 +7,35 @@ var submitElement = document.getElementById("submit");
 var throwDiceElement = document.getElementById("dado");
 var addEmailElement = document.getElementById("add");
 var inputEmail;
-var exit=1;//imposto exit ad uno per evitare di aggiungere indirizzi email non ancora ricercati
+var found=1;//imposto found ad uno per evitare di aggiungere indirizzi email non ancora ricercati
 var yourDice;
 var computerDice;
+var i;
 
 // ACCESSO TRAMITE MAIL
 submitElement.addEventListener("click",function(){
 
   inputEmail = document.getElementById("mail").value;
-  exit = 0; //permetto al ciclo di proseguire finchè non trova un match
-  console.log(inputEmail,typeof inputEmail, arrayEmail[0],typeof arrayEmail[0],exit);
+  found = 0; //permetto al ciclo di proseguire finchè non trova un match
+  console.log(inputEmail,typeof inputEmail, arrayEmail[0],typeof arrayEmail[0],found);
 
   // CICLO FOR CHE CONTROLLA OGNI POSIZIONE DELL'ARRAY IN CERCA DI UN INDIRIZZO EMAIL CORRISPONDENTE, ESCE DAL CICLO QUANDO LO TROVA
-  for(i=0 ; i<=arrayEmail.length && exit==0; i++){
+  for(i=0 ; i<=arrayEmail.length; i++){
     if (inputEmail == arrayEmail[i]){
-      document.getElementById("message").innerHTML = "Il suo indirizzo email è presente nel database; può accedere!";
-      exit=1; //esco dal ciclo
-    }else {
-      document.getElementById("message").innerHTML = "Il suo indirizzo email non è presente nel database; NON può accedere!";
+      found=1; //esco dal ciclo
     }
+  }
+
+  if(found==1){
+    document.getElementById("message").innerHTML = "Il suo indirizzo email è presente nel database; può accedere!";
+  } else {
+    document.getElementById("message").innerHTML = "Il suo indirizzo email non è presente nel database; NON può accedere!";
   }
 });
 
 // REGISTRARSI TRAMITE EMAIL NON PRESENTE
 addEmailElement.addEventListener("click",function(){
-  if(exit==0){
+  if(found==0){
     arrayEmail.push(inputEmail);
     console.log(arrayEmail);
   }
@@ -39,13 +43,19 @@ addEmailElement.addEventListener("click",function(){
 
 // LANCIO DEL DADO
 throwDiceElement.addEventListener("click",function(){
-
+  yourDice=computerDice;
+  console.log(yourDice, computerDice);
   // CICLO FOR CHE SIMULA DUE LANCI DI DADI E RIPETE IL LANCIO FINCHè NON SONO DIVERSI
   for(i=0;yourDice == computerDice;i++){
     yourDice = Math.floor(Math.random() * DICE_MAX + DICE_MIN);
     computerDice = Math.floor(Math.random() * DICE_MAX + DICE_MIN);
     console.log(yourDice, computerDice);
   }
+
+  // yourDice = Math.floor(Math.random() * DICE_MAX + DICE_MIN);
+  // computerDice = Math.floor(Math.random() * DICE_MAX + DICE_MIN);
+  // console.log(yourDice, computerDice);
+
   // VISUALIZZAZIONE LANCI
   document.getElementById("mio-lancio").innerHTML = "Il tuo lancio: " + yourDice;
   document.getElementById("pc-lancio").innerHTML = "Il lancio del PC: " + computerDice;
@@ -53,8 +63,9 @@ throwDiceElement.addEventListener("click",function(){
   // ELABORAZIONE ESITO DEL LANCIO E VISUALIZZAZIONE
   if(yourDice>computerDice){
     document.getElementById("result").innerHTML = "VITTORIA";
-  } else {
+  } else if(yourDice<computerDice){
     document.getElementById("result").innerHTML = "SCONFITTA, ritenta!";
+  } else {
+    document.getElementById("result").innerHTML = "PAREGGIO, ritenta!";
   }
-
 });
